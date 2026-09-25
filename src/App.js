c9f199043c5216e7840c2370840c02f8af5535dd
domenico2003 <domdat03@gmail.com>
2026-09-25 17:34:08 +0200

    create le paggine e impostato il router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/*Questo file è utilizzato per impostare le diverse pagine di cui il sito è composto.
+ Questa funzionalità è resa possibile grazie alla libreria react-router-dom 
+ i cui componenti sono importati di seguito */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Di seguito importo i diversi fali jsx che poi diventeranno le vere e proprie paginedel sito
+import { Home } from "./components/pages/home";
+import { ChiSiamo } from "./components/pages/chiSiamo";
+import { Ambiente } from "./components/pages/ambiente";
+import { Persone } from "./components/pages/persone";
+import { Report } from "./components/pages/report";
+
+// Qui importo la navbar e il footer, elementi presenti in ogni pagina quindi li implemento direttamente in questo file
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
+
+// infine importo il css per lo stile
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //qui apro il browserrouter che permette di gestire le diverse rotte senza ricaricare la pagina
+    <BrowserRouter>
+      {/*qui inserisco la navbar da mostrare in tutte le pagine */}
+      <Navbar />
+      {/*di seguito indico tutte le rotte e ogni tag route indica la singola rotta*/}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chi-siamo" element={<ChiSiamo />} />
+        <Route path="/ambiente" element={<Ambiente />} />
+        <Route path="/persone" element={<Persone />} />
+        <Route path="/Report" element={<Report />} />
+      </Routes>
+      {/*qui inserisco il footer da mostrare in tutte le pagine */}
+      <Footer />
+    </BrowserRouter>
   );
 }
 
+//qui esporto la funzione che mi contiene l'intera app per poi importarla su index.js
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//di seguito importo due elementi della libreria react router dom che mi permettono di usufruire della paginazione
+import { BrowserRouter } from "react-router";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//importo il css
+import "./index.css";
+
+//infine importo il file app.js che contiene l'intera applicazione
+import App from "./App";
+
+/*di seguito in una costante mi salvo il punto dell'html selezionato tramite id
+ su cui poi far girare la mia applicazione react */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//utilizzo render per far apparire il tutto sulla pagina nel browser utilizzato per far girare l'applicazione
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
